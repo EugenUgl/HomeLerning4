5f8c9b9c2fe49c982d4f20a073ef4ed6823e1647
EugenUgl <ewgenugl@gmail.com>
2023-01-23 21:24:09 +0300

    ДЗ 4

---
+++ BoobleSort/View/Program.cs
@@ -0,0 +1,47 @@
+﻿using BoobleSort_;
+
+namespace View_
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array size: ");
+            int arrSize;
+            View.InputValue(Console.ReadLine(), out arrSize);
+
+            Console.Write("Enter higth range: ");
+            int hiRange;
+            View.InputValue(Console.ReadLine(), out hiRange);
+
+            Console.Write("Enter low range: ");
+            int loRange;
+            View.InputValue(Console.ReadLine(), out loRange);
+
+            Console.Write("Creaated array: ");
+            BoobleSort boobleSort = new();
+
+            if (loRange > hiRange)
+            {
+                Console.WriteLine("Error: ");
+                return;
+            }
+
+            var arr = boobleSort.CreateArrayRandom(arrSize, loRange, hiRange);
+            View.OutArr(arr);
+
+            Console.Write("Sorted ascending array: ");
+            arr = boobleSort.SortArray(arr, true);
+            View.OutArr(arr);
+
+            //Descending
+            Console.Write("Sorted descending array: ");
+            arr = boobleSort.SortArray(arr, false);
+            View.OutArr(arr);
+
+            Console.WriteLine($"Sorted descending array: {BoobleSort.NumberOfExchangeOperations}");
+        }
+    }
+}
---
+++ BoobleSort/View/View.cs
@@ -0,0 +1,16 @@
+﻿namespace View_
+{
+    static class View
+    {
+        public static bool InputValue(string? strValue, out int value)
+            => int.TryParse(strValue, out value);
+        
+
+        public static void OutArr(int[] arr)
+        {
+            foreach (var item in arr) 
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
+}
